ad5d736ebaf44d8619ea0f3fedab72f03a070f87
Cristian Espes <christian_zgza@hotmail.com>
2020-01-25 11:26:15 +0100

    First commit

---
+++ grupohasten/Applitaction/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  grupohasten
+//
+//  Created by Cristian Espes on 25/01/2020.
+//  Copyright © 2020 Cristian Espes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
